d1058f4591b6faa1820a0d8a6b7328eae92496e6
Aleksandr Melikhov <melalecks@yandex.ru>
2025-08-27 21:45:02 +0300

    1.1

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -6,28 +6,29 @@ from pytest_django.asserts import assertRedirects
 
 @pytest.mark.django_db
 @pytest.mark.parametrize('parametrized_client, url, method, status', (
-        (pytest.lazy_fixture('client'), pytest.lazy_fixture('login_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('client'), pytest.lazy_fixture('logout_url'),
-         'post', HTTPStatus.OK),
-        (pytest.lazy_fixture('client'), pytest.lazy_fixture('signup_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('client'), pytest.lazy_fixture('homepage_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('client'), pytest.lazy_fixture('news_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('log_author'), pytest.lazy_fixture('edit_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('log_author'), pytest.lazy_fixture('delete_url'),
-         'get', HTTPStatus.OK),
-        (pytest.lazy_fixture('log_reader'), pytest.lazy_fixture('edit_url'),
-         'get', HTTPStatus.NOT_FOUND),
-        (pytest.lazy_fixture('log_reader'), pytest.lazy_fixture('delete_url'),
-         'get', HTTPStatus.NOT_FOUND),
-    ))
+    (pytest.lazy_fixture('client'), pytest.lazy_fixture('login_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('client'), pytest.lazy_fixture('logout_url'),
+     'post', HTTPStatus.OK),
+    (pytest.lazy_fixture('client'), pytest.lazy_fixture('signup_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('client'), pytest.lazy_fixture('homepage_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('client'), pytest.lazy_fixture('news_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('log_author'), pytest.lazy_fixture('edit_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('log_author'), pytest.lazy_fixture('delete_url'),
+     'get', HTTPStatus.OK),
+    (pytest.lazy_fixture('log_reader'), pytest.lazy_fixture('edit_url'),
+     'get', HTTPStatus.NOT_FOUND),
+    (pytest.lazy_fixture('log_reader'), pytest.lazy_fixture('delete_url'),
+     'get', HTTPStatus.NOT_FOUND),
+))
 def test_pages_avaiiability(parametrized_client, url, method, status):
     response = getattr(parametrized_client, method)(url)
     assert response.status_code == status
+
 
 @pytest.mark.parametrize('url, redirect_url', (
     (pytest.lazy_fixture('edit_url'), pytest.lazy_fixture('login_url')),
